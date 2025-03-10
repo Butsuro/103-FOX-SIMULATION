92add1ff5f77ba768e57b56120773bc2077abe1b
ElfinHydra6 <124932957+ElfinHydra6@users.noreply.github.com>
2025-03-10 12:34:45 -0400

    Commit

--- BlankBrain.py
+++ BlankBrain.py
@@ -1,11 +1,24 @@
 import random
-
+import Masterarray as MA
 
 def turn(Level_sleep, Level_hunger):
     Level_sleep = Level_sleep + 0.2
     Level_hunger = Level_hunger + 0.2
 
+def DenQuantReached(family_num, array):
+    count = 0
+    for row in array[0]:
+        for num in row:
+            if num == family_num:
+                count += 1
+    
+    if count >= 3:
+        return True
+    else:
+        return False
+
 
 def BrainMove():
-    hunger = 0
-    sleep = 0
+    hunger = 1
+    sleep = 1
+    Denning = 1
