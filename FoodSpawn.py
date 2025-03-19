a88e3d45481b0273d7a421599cac46ff5f6b1b4c
ElfinHydra6 <124932957+ElfinHydra6@users.noreply.github.com>
2025-03-19 02:45:04 -0400

    making sure things dont spawn over each ot

--- FoodSpawn.py
+++ FoodSpawn.py
@@ -1,23 +1,17 @@
 import Masterarray as MA 
 import random
-import numpy as np
-
+import numpy as np 
 def spawnitems(array_3d, num_items, layer, item_type):
-
-    array_3d[layer][1][1] = 1
-
+ 
     valid_positions = [(i, j) for i in range(array_3d.shape[1]) 
                    for j in range(array_3d.shape[2]) 
-                   if array_3d[0][i][j] not in {0, 4, 7}]
-    
+                   if (array_3d[0][i][j] not in {0, 4, 7}) and (array_3d[layer][i][j] == 0)]
+     
     if len(valid_positions) < num_items:
-        return array_3d
-    
+        return
+     
     else:
         selected_spots = random.sample(valid_positions, num_items)
-
+ 
         for i,j in selected_spots:
             array_3d[layer][i][j] = item_type
-        
-        return array_3d
-    
