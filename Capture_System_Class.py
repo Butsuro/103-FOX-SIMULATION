a83f372f2528a2b6639430c7d5f7198f167fb3d5
GMartens5 <24jrr@queensu.ca>
2025-03-21 13:15:50 -0400

    Update and rename Capture System Class to Capture_System_Class.py

--- Capture_System_Class.py
+++ Capture_System_Class.py
@@ -2,21 +2,23 @@ class captureSystem:
     def __init__(self, length, width, height, pos):
         self.length = length
         self.width = width
-        self.width = height
+        self.height = height
         self.pos = np.array(pos)
         self.totalCaptures = 0
         self.correctCaptures = 0
-        self.capturePercent = 0
+        self.correctCapturePercent = 0
+        self.capturedCanids = []
         
     def captureCanid(self, masterArray, foxList, targetCanid):
         x = round(self.pos[0])
         y = round(self.pos[1])
         if masterArray[1][y][x] != 0:
             capturedCanid = masterArray[1][y][x]
-            totalCaptures += 1
+            self.totalCaptures += 1
             #counter variable from move function
             captureTime = counter
-            foxList.pop(capturedCanid-1)
+            foxList.remove(capturedCanid)
+            self.capturedCanids.append([capturedCanid, captureTime])
             if capturedCanid == targetCanid:
-                correctCaptures += 1
-                correctCapturePercent = correctCapture/capturePercent
+                self.correctCaptures += 1
+                self.correctCapturePercent = self.correctCaptures/self.totalCaptures
