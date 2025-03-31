648e0c1c8468bca32bbacb7a493fde0410b89a2a
ElfinHydra6 <124932957+ElfinHydra6@users.noreply.github.com>
2025-03-31 19:20:32 -0400

    terminal printing

--- Initializer.py
+++ Initializer.py
@@ -122,14 +122,15 @@ if not skip_trap_sim:
                             if (Canid.family == homie.family):
                                 homie.family_count = homie.family_size -1
                         CanidAgentList.remove(Canid)
+                        print(f"trap at {round(Canid.pos[1])}{round(Canid.pos[0])} (x,y) took {counter/cycle_multiplier * 24} hours to capture the canids")
                         if break_after_one:
                             break
                         pbar.update(1)
                     else:
                         Master_array = Canid.move(Master_array, CanidAgentList)
     print("capture sim complete")
-    print("total time taken is:") 
-    print(counter/cycle_multiplier)
+    print("total time taken to capture is:") 
+    print(f"{counter/cycle_multiplier} days")
 
 
 print("sending results")
